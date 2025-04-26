e8fc85283463099a497dcfa5b62f164a00ce470b
Konstantinos <konstantinos.xr96@gmail.com>
2025-04-26 18:17:47 +0300

    cleaned up avro utils

--- pcomp_utils/avro_layer_schema.py
+++
@@ -1,9 +0,0 @@
-layer_message_schema = {
-    "type": "record",
-    "name": "LayerMessage",
-    "fields": [
-        {"name": "layer_id", "type": "string"},
-        {"name": "image_id", "type": "int"},
-        {"name": "data", "type": "bytes"}
-    ]
-}
