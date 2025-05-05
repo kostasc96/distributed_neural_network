8e795369403f320a265cb1da4d55065d3b71453e
Konstantinos <konstantinos.xr96@gmail.com>
2025-05-06 02:31:10 +0300

    updated streams intermediate script

--- streams_functions/streams_intermediate/src/main/scala/app/AkkaLayer0.scala
+++ streams_functions/streams_intermediate/src/main/scala/app/AkkaLayer0.scala
@@ -2,128 +2,136 @@ package app
 
 import akka.actor.ActorSystem
 import akka.dispatch.MessageDispatcher
-import akka.kafka.scaladsl.{Consumer, Producer}
-import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
-import akka.stream.{ActorAttributes, Materializer, OverflowStrategy, SystemMaterializer}
+import akka.kafka.scaladsl.{Committer, Consumer, Producer}
+import akka.kafka.{CommitterSettings, ConsumerSettings, ProducerMessage, ProducerSettings, Subscriptions}
+import akka.kafka.ConsumerMessage.{CommittableOffset, CommittableOffsetBatch}
+import akka.stream.{ActorAttributes, KillSwitches, Materializer, OverflowStrategy, SystemMaterializer}
+import akka.stream.scaladsl.{Keep, Sink}
 import com.typesafe.config.ConfigFactory
 import io.lettuce.core.RedisClient
 import io.lettuce.core.api.StatefulRedisConnection
 import io.lettuce.core.api.async.RedisAsyncCommands
 import io.lettuce.core.codec.ByteArrayCodec
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.{ProducerConfig, ProducerRecord}
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 
-import java.nio.charset.StandardCharsets
 import java.nio.{ByteBuffer, ByteOrder}
-import scala.collection.mutable.ListBuffer
-import scala.concurrent._
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
 object AkkaLayer0 extends App {
-  // 1) Load all settings from src/main/resources/application.conf
+  // === 1) Config & System ===
   val config = ConfigFactory.load()
+  implicit val system: ActorSystem       = ActorSystem("streams-intermediate", config)
+  implicit val mat: Materializer         = SystemMaterializer(system).materializer
+  implicit val ec: ExecutionContext      = system.dispatcher
+  // additional dispatchers
+  val computeDispatcher: ExecutionContext = system.dispatchers.lookup("akka.actor.compute-dispatcher")
 
-  implicit val system: ActorSystem  = ActorSystem("streams-intermediate", config)
-  implicit val mat: Materializer    = SystemMaterializer(system).materializer
-  implicit val ec: ExecutionContext = system.dispatcher
-
-  private val redisDispatcher: MessageDispatcher =
-    system.dispatchers.lookup("akka.actor.redis-dispatcher")
-
-  // 2) Read “flat” config values
-  val neuronCount   = config.getInt("akka.stream.neuron-count")
-  val parallelism   = config.getInt("akka.stream.parallelism")
-  val bufferSize    = config.getInt("akka.stream.buffer-size")
-  val batchSize     = config.getInt("akka.stream.batch-size")
-  val batchDuration = config.getString("akka.stream.batch-duration") match {
-    case s if s.endsWith("ms") => s.stripSuffix("ms").toInt.millis
-    case s if s.endsWith("s")  => s.stripSuffix("s").toInt.seconds
-    case _                      => throw new IllegalArgumentException("Invalid batch-duration")
-  }
-  val redisTtlSec   = config.getInt("akka.stream.redis-ttl-sec")
-
-  val inputTopic   = config.getString("myapp.kafka.input-topic")
-  val outputTopic  = config.getString("myapp.kafka.output-topic")
-  val kafkaServers = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", throw new IllegalArgumentException(s"Environment variable KAFKA_BOOTSTRAP_SERVERS not set"))
-  val redisUri = sys.env.getOrElse("REDIS_URI", throw new IllegalArgumentException(s"Environment variable REDIS_URI not set"))
-
-  // 3) Redis setup
-  private val client: RedisClient                               = RedisClient.create(redisUri)
+  // === 2) Redis async setup ===
+  val redisUri    = sys.env("REDIS_URI")
+  val neuronCount = config.getInt("akka.stream.neuron-count")
+  val redisTtlSec = config.getInt("akka.stream.redis-ttl-sec")
+  private val client: RedisClient = RedisClient.create(redisUri)
   private val asyncConn: StatefulRedisConnection[Array[Byte], Array[Byte]] = client.connect(ByteArrayCodec.INSTANCE)
-  private val asyncCmds: RedisAsyncCommands[Array[Byte], Array[Byte]]     = asyncConn.async()
+  private val asyncCmds: RedisAsyncCommands[Array[Byte], Array[Byte]] = asyncConn.async()
   asyncCmds.setAutoFlushCommands(false)
-  private val syncConn                                    = client.connect(ByteArrayCodec.INSTANCE)
-  private val syncCmds                                    = syncConn.sync()
+  // custom dispatcher for Redis operations
+  val redisDispatcher: ExecutionContext = system.dispatchers.lookup("akka.actor.redis-dispatcher")
 
-  private def doubleBytes(d: Double): Array[Byte] = {
+  def doubleBytes(d: Double): Array[Byte] = {
     val buf = ByteBuffer.allocate(java.lang.Double.BYTES).order(ByteOrder.LITTLE_ENDIAN)
     buf.putDouble(d).array()
   }
 
-  // 4) Kafka settings
+  // === 3) Kafka settings ===
+  val inputTopic   = config.getString("myapp.kafka.input-topic")
+  val outputTopic  = config.getString("myapp.kafka.output-topic")
+  val kafkaServers = sys.env("KAFKA_BOOTSTRAP_SERVERS")
   val consumerSettings = ConsumerSettings(system, new StringDeserializer, new StringDeserializer)
     .withBootstrapServers(kafkaServers)
     .withGroupId("akka-streams0-group")
-    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,  "latest")
-    .withProperty(ConsumerConfig.FETCH_MIN_BYTES_CONFIG,    bufferSize.toString)
-    .withProperty(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG,  "100")
+    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
 
-  val producerSettings = ProducerSettings(system, new StringSerializer, new StringSerializer)
+  val producerSettings  = ProducerSettings(system, new StringSerializer, new StringSerializer)
     .withBootstrapServers(kafkaServers)
-    .withProperty(ProducerConfig.LINGER_MS_CONFIG,           "5")
-    .withProperty(ProducerConfig.BATCH_SIZE_CONFIG,         (2 * 1024 * 1024).toString)
-    .withProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG,   "lz4")
-    .withProperty(ProducerConfig.ACKS_CONFIG,               "1")
+  val committerSettings = CommitterSettings(system)
+    .withMaxBatch(50)
+    .withMaxInterval(2.seconds)
 
-  // 5) Stream graph
-  Consumer
-    .plainSource(consumerSettings, Subscriptions.topics(inputTopic))
+  val parallelism = config.getInt("akka.stream.parallelism")
+  val bufferSize  = config.getInt("akka.stream.buffer-size")
+  val batchSize   = config.getInt("akka.stream.batch-size")
+  val batchDur    = config.getString("akka.stream.batch-duration") match {
+    case s if s.endsWith("ms") => s.dropRight(2).toInt.millis
+    case s if s.endsWith("s")  => s.dropRight(1).toInt.seconds
+    case _ => throw new IllegalArgumentException("Invalid batch-duration")
+  }
+
+  // === 4) Stream ===
+  Consumer.committableSource(consumerSettings, Subscriptions.topics(inputTopic))
     .withAttributes(ActorAttributes.dispatcher("akka.actor.kafka-consumer-dispatcher"))
+    .viaMat(KillSwitches.single)(Keep.right)
     .map { msg =>
-      val v   = msg.value()
+      val v   = msg.record.value()
       val sep = v.indexOf('|')
-      (msg.key(), (v.substring(0, sep).toInt, v.substring(sep + 1).toDouble))
+      (msg.committableOffset, msg.record.key(), v.substring(0, sep).toLong, v.substring(sep + 1).toDouble)
     }
     .buffer(bufferSize, OverflowStrategy.backpressure)
-    .groupedWithin(batchSize, batchDuration)
+    .groupedWithin(batchSize, batchDur)
     .mapAsyncUnordered(parallelism) { batch =>
       Future {
-        val completed = ListBuffer.empty[String]
-        val futures = batch
-          .groupBy(_._1)
+        import scala.collection.mutable.ListBuffer
+        // Prepare Redis pipeline
+        asyncCmds.setAutoFlushCommands(false)
+
+        // Track (id, lastOffset, incrFuture)
+        val updates = batch
+          .groupBy(_._2)
           .map { case (id, elems) =>
-            val finalKey = s"0_$id".getBytes(StandardCharsets.UTF_8)
-            val countKey = s"cnt:0:$id".getBytes(StandardCharsets.UTF_8)
-            elems.foreach { case (_, (idx, dbl)) =>
-              asyncCmds.setrange(finalKey, idx * 8L, doubleBytes(dbl))
+            val dataKey  = s"0_$id".getBytes
+            val countKey = s"cnt:0:$id".getBytes
+            elems.foreach { case (_, _, idx, dbl) =>
+              asyncCmds.setrange(dataKey, idx * 8L, doubleBytes(dbl))
             }
-            val incrFut = asyncCmds.incrby(countKey, elems.size)
-            (id, finalKey, countKey, incrFut)
+            val incrF = asyncCmds.incrby(countKey, elems.size)
+            // Return tuple of id, last offset, and a tuple of keys/future
+            (id, elems.last._1, (dataKey, countKey, incrF))
           }
           .toList
 
+        // Flush all commands at once
         asyncCmds.flushCommands()
-        futures.foreach { case (id, finalKey, countKey, incrFut) =>
-          val cnt = incrFut.get()
-          if (cnt >= neuronCount) {
-            asyncCmds.expire(finalKey, redisTtlSec)
+
+        // Collect completed ids
+        val completed = ListBuffer.empty[(String, CommittableOffset)]
+        updates.foreach { case (id, offset, data) =>
+          val (dataKey, countKey, incrF) = data.asInstanceOf[(Array[Byte], Array[Byte], io.lettuce.core.RedisFuture[java.lang.Long])]
+          if (incrF.get() >= neuronCount) {
+            asyncCmds.expire(dataKey, redisTtlSec)
             asyncCmds.del(countKey)
-            completed += id
+            completed += ((id, offset))
           }
         }
-        asyncCmds.flushCommands()
+        // Return list of completed (id, offset)
         completed.toList
       }(redisDispatcher)
     }
     .mapConcat(identity)
-    .withAttributes(ActorAttributes.dispatcher("akka.actor.compute-dispatcher"))
-    .map(id => new ProducerRecord[String, String](outputTopic, id, id))
-    .runWith(Producer.plainSink(producerSettings.withDispatcher("akka.actor.compute-dispatcher")))
-    .onComplete { _ =>
-      asyncConn.close()
-      syncConn.close()
-      client.shutdown()
-      system.terminate()
+    .map { case (id, offset) =>
+      // emit to Kafka
+      ProducerMessage.single(new ProducerRecord[String, String](outputTopic, id, id), offset)
     }
+    .via(Producer.flexiFlow(producerSettings)) // you can attach a producer dispatcher if desired (e.g. .withAttributes(ActorAttributes.dispatcher("akka.actor.kafka-producer-dispatcher")))
+    .map(_.passThrough)
+    .toMat(Committer.sink(committerSettings))(Keep.both)
+    .run()
+
+  // === 5) Shutdown hook ===
+  sys.addShutdownHook {
+    asyncConn.close()
+    client.shutdown()
+    system.terminate()
+  }
 }
